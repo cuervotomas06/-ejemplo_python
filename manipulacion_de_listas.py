e907fd0e7dcfcbd1fb33f37ebd9ba55f108e16d5
Tomas Amaya <cuervotomas06@gmail.com>
2026-09-09 12:42:57 -0300

    champagnat

--- manipulacion_de_listas.py
+++ manipulacion_de_listas.py
@@ -1,6 +1,6 @@
 lista = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 num = int(input("Ingrese un numero: "))
-
+print("Numeros multiplos de:", num)
 listaMultiplos = []
 listaNoMultiplos = []
 for i in lista:
